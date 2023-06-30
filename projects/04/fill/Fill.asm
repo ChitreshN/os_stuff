3231bf7990fa55df83ea039112f1bcb6e0ece60d
ChitreshN <chitreshnarra@gmail.com>
2023-07-01 00:11:32 +0530

    Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,85 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(LOOP)
+    @KBD
+    D = M
+    @WORK
+    D;JNE
+    @LOOP
+    0;JMP
+(WORK)
+    @i
+    M = 1
+    //pdt = 0
+    @R2
+    M = 0
+    @256
+    D = A
+    @R0
+    M = D
+    @32
+    D = A
+    @R1
+    M = D
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(LOOP1)
+// R1 iterations
+//
+    @i
+    D = M
+    @R1
+    D = D - M
+    @STOP
+    D ; JGT
+    @R0
+    D = M
+    @R2
+    M = M + D
+    @i
+    M = M+1
+    @LOOP1
+    0;JMP
+(STOP)
+@j
+M = 1
+@SCREEN
+D = A
+@curword
+M = D
 
-// Put your code here.
+(DARK)
+    @j
+    D = M
+    @R2
+    D = D - M
+    @LOOP
+    D;JGT
+    @curword
+    D = M
+    @j
+    D = D + M
+    A = D
+    M = -1
+    @j
+    M = M + 1
+    @DARK
+    0;JMP
+
+(END)
+    @END
+    0 ; JMP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

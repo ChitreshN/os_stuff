5bf93b45a9f51991622af8e6c9bb3075b015497f
ChitreshN <chitreshnarra@gmail.com>
2023-07-07 09:56:07 +0530

    Add line processing to assembler

--- projects/06/assembler.c
+++ projects/06/assembler.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define buffer_max 128
+#define bits 16
+
 struct jump{
     const char* nothing;
     const char* JGT;
@@ -13,7 +16,7 @@ struct jump{
     const char* JMP;
 };
 
-struct jump const jmp_adr = {
+struct jump const jmp = {
     .nothing = "000",
     .JGT = "001",
     .JEQ = "010",
@@ -46,19 +49,90 @@ struct dest const dest_adr= {
     .AMD = "111",
 };
 
-#define buffer_max 128
+struct comp{
+    const char* _0;
+    const char* _1;
+    const char* _neg_1;
+    const char* D;
+    const char* A;
+    const char* not_D;
+    const char* not_A;
+    const char* neg_D;
+    const char* neg_A;
+    const char* D_p_1;
+    const char* A_p_1;
+    const char* D_m_1;
+    const char* A_m_1;
+    const char* D_p_A;
+    const char* D_m_A;
+    const char* A_m_D;
+    const char* D_and_A;
+    const char* D_or_A;
+};
+
+struct comp const comp_code = {
+    ._0 = "101010",
+    ._1 = "111111",
+    ._neg_1 = "111010",
+    .D = "001100",
+    .A = "110000",
+    .not_D = "001101",
+    .not_A = "110001",
+    .neg_D = "001111",
+    .neg_A = "110011",
+    .D_p_1 = "011111",
+    .A_p_1 = "110111",
+    .D_m_1 = "001110",
+    .A_m_1 = "110010",
+    .D_p_A = "000010",
+    .D_m_A = "010011",
+    .A_m_D = "000111",
+    .D_and_A = "000000",
+    .D_or_A = "010101",
+};
+
+char* process_line(char code[bits],int n, char line[n+1]){
+     
+    for (int i = 0;i < bits;i++){
+        code[i] = '0';
+    }
+    // check for = or ; in line
+    int eq = -1;
+    for(int i = 0;i < n;i++){
+        if (line[i] == '='){
+            eq = 1;
+            break;
+        }
+        else if(line[i] == ';'){
+            eq = 0;
+            break;
+        }
+        else if(line[i] == '@'){
+            eq = 2;
+            break;
+        }
+    }
+
+    // if eq = 1 then the line is a computation
+    // if eq = 0 then it's a jump statement
+    // if eq = 2 then it's a @value type instruction
+    
+    return code;
+}
 
 
 int main(int argc, char* argv[argc+1]){
-    FILE* logfile = fopen("mylog.txt", "a");
-    FILE* progfile = fopen("max/MaxL.asm","r");
+   // FILE* progfile = fopen("max/MaxL.asm","r");
     char buffer[buffer_max] = {0};
-    if (progfile){
-        while(fgets(buffer, buffer_max, progfile)){
-            if(buffer[0] != '/' && !isspace(buffer[0]))  fputs(buffer, stdout);
-        }
-        fclose(progfile);
-    }
+    //if (progfile){
+    //    while(fgets(buffer, buffer_max, progfile)){
+    //        if(buffer[0] != '/' && !isspace(buffer[0]))  fputs(buffer, stdout);
+    //    }
+    //    fclose(progfile);
+    //}
+    char code[bits];
+    char* processed = process_line(code, 3, "==12");
+    printf("%s",processed);
     return 0;
 }
  
